50a90648dd8954ba5b8d1bb53c31656d04f93930
gui-brant <guibrant.ca@gmail.com>
2025-06-06 13:22:54 -0400

    adding motion of the player's spaceship

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 import pygame
 class Player(CircleShape): #don't forget your "self."s
     def __init__(self, x, y): 
@@ -15,3 +15,12 @@ class Player(CircleShape): #don't forget your "self."s
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
