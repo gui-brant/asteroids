b264de49949c04d82148fd755bf6efe8e568db67
gui-brant <guibrant.ca@gmail.com>
2025-06-06 13:11:24 -0400

    created the player.py file (on the last commit actually) and added it to the main.py file, so that the player's triangle character shows up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import * 
+from player import Player
 
 def main():
         print("Starting Asteroids!")
@@ -12,13 +13,17 @@ def main():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create a GUI window with the specified dimensions
         pygame.time.Clock()  # Create a clock object to control the frame rate
         dt = 0  # Initialize delta time variable
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)  # Create a player object at the center of the screen
+        # Main game loop
         while True:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return 
                 screen.fill("black") #fills the screen with black color
-                pygame.display.flip()  # Update the full display Surface to the screen --> must be called last in the loop
+                player.draw(screen) #the player has to pop up after the screen pops up but before the frame is refreshed
                 dt = pygame.time.Clock().tick(60)/1000 # .tick() returns the number of milliseconds since the last call, so we divide by 1000 to get seconds, meaning the time for 1/60th of a second, or a frame
+                pygame.display.flip()  # Update the full display Surface to the screen --> must be called last in the loop
+                
                 
 
 
