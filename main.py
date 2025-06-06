eb35f150064f7c4ba3a7b0d5064e65cbfbd5fe67
gui-brant <guibrant.ca@gmail.com>
2025-06-06 13:01:22 -0400

    added the CircleShape.py file to represent objects on the screen

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ def main():
         print(f"Screen height: {SCREEN_HEIGHT}")  
         pygame.init() # Initialize all imported pygame modules
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create a GUI window with the specified dimensions
+        pygame.time.Clock()  # Create a clock object to control the frame rate
+        dt = 0  # Initialize delta time variable
         while True:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return 
                 screen.fill("black") #fills the screen with black color
                 pygame.display.flip()  # Update the full display Surface to the screen --> must be called last in the loop
- 
+                dt = pygame.time.Clock().tick(60)/1000 # .tick() returns the number of milliseconds since the last call, so we divide by 1000 to get seconds, meaning the time for 1/60th of a second, or a frame
+                
+
+
 
 if __name__ == "__main__":
     main()
